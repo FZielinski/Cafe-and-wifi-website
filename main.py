51df55ad13bc1abe2cf6afb6e11776057484434f
FZielinski <85953382+FZielinski@users.noreply.github.com>
2022-03-24 19:33:23 +0100

    Register form added

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from forms import RegisterForm
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy(app)
 
@@ -25,6 +29,38 @@ class Cafe(db.Model):
 def get_all_cafes():
     cafes = Cafe.query.all()
     return render_template("index.html", all_cafes=cafes)
+
+
+@app.route("/cafe/<int:cafe_id>", methods=["GET", "POST"])
+def show_cafe(cafe_id):
+    cafe = Cafe.query.get(cafe_id)
+    return render_template("cafe.html", cafe=cafe)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    new_user = RegisterForm()
+    return render_template("register.html", form=new_user)
+
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    return render_template("logout.html")
+
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == "__main__":
