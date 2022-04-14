624cc48192d011e7718faf3e75af95094ae5d085
FZielinski <85953382+FZielinski@users.noreply.github.com>
2022-04-14 20:59:21 +0200

    Registration works

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegisterForm
 from flask_bootstrap import Bootstrap
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 
 app = Flask(__name__)
@@ -9,6 +13,15 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 db = SQLAlchemy(app)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    comments = relationship("Comment", back_populates="comment_author")
 
 
 class Cafe(db.Model):
@@ -23,6 +36,19 @@ class Cafe(db.Model):
     can_take_calls = db.Column(db.Boolean)
     seats = db.Column(db.String(250))
     coffee_price = db.Column(db.String(250))
+    comments = relationship("Comment", back_populates="parent_cafe")
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    cafe_id = db.Column(db.Integer, db.ForeignKey("cafe.id"))
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    parent_cafe = relationship("Cafe", back_populates="comments")
+    comment_author = relationship("User", back_populates="comments")
+    text = db.Column(db.Text, nullable=False)
+
+db.create_all()
 
 
 @app.route('/')
@@ -44,8 +70,24 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    new_user = RegisterForm()
-    return render_template("register.html", form=new_user)
+    form = RegisterForm()
+    if form.validate_on_submit():
+
+        salted_password = generate_password_hash(
+            form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+            email=form.email.data,
+            name=form.name.data,
+            password=salted_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for("get_all_cafes"))
+
+    return render_template("register.html", form=form)
 
 
 @app.route("/logout", methods=["GET", "POST"])
